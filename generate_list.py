87c3af910fdeda520d7175a654f586be27aeab66
asolee <85689006+asolee@users.noreply.github.com>
2022-09-29 16:12:58 +0200

    input generalization

--- generate_list.py
+++ generate_list.py
@@ -3,17 +3,24 @@
 #libraries
 import os
 
-#directory
-dir = "/users/rg/gasole/DEG/sample_collapsed"
+#directory for the fastq input
+path = os.getcwd()
+dir = path+"/sample_input"
 
 #final list
 list = open("input_file_list.txt",'w')
 
 #create the list
+sample_list = []
 for sample in os.listdir(dir):
-   sample_name = sample[:3]
-   R1 = dir+"/"+sample_name+"/merge_R1.fastq.gz"
-   R2 = dir+"/"+sample_name+"/merge_R2.fastq.gz"
+   sample = sample.split('_')[0]
+   if sample not in sample_list:
+      sample_list.append(sample)
+
+#write the input file for the parallel job
+for sample_name in sample_list:
+   R1 = dir+"/"+sample_name+"_R1.fastq.gz"
+   R2 = dir+"/"+sample_name+"_R2.fastq.gz"
    line = sample_name+"\t"+R1+"\t"+R2+"\n"
    list.write(line)
 list.close()
